261f7af1ec01e7676ab48fbf43243d207ab4e8c7
wnals9 <wnals16@naver.com>
2023-09-25 12:47:08 +0900

    20230925

--- 01_IoC/src/main/java/com/gdu/app01/xml01/MainWrapper.java
+++ 01_IoC/src/main/java/com/gdu/app01/xml01/MainWrapper.java
@@ -1,12 +1,13 @@
 package com.gdu.app01.xml01;
 
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainWrapper {
-
-  public static void main(String[] args) {
-
+  
+  public static void ex01() {
+    
     // app-context.xml 파일을 읽기 (여기에서 <bean> 태그로 정의해 둔 객체가 생성된다.)
     AbstractApplicationContext ctx = new GenericXmlApplicationContext("xml01/app-context.xml");
     
@@ -22,6 +23,30 @@ public class MainWrapper {
     // app-context.xml 파일 닫기
     ctx.close();
     
+  }
+
+  public static void ex02() {
+    
+    // app-context.xml 파일 읽어서 <bean> 태그에 정의된 객체 만들기
+    AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("xml01/app-context.xml");
+    
+    // 객체 가져오기(man, woman) 2가지 방법
+    Person man = (Person)ctx.getBean("man");
+    Person woman = ctx.getBean("woman", Person.class); 
+    
+    // 객체 확인
+    System.out.println(man.getName() + ", " + man.getAge());
+    man.getCalculator().add(1, 2);
+    System.out.println(woman.getName() + ", " + woman.getAge());
+    woman.getCalculator().add(3, 4);
+    
+    // app-context.xml 파일 닫기
+    ctx.close();
+    
+  }
+  
+  public static void main(String[] args) {
+    ex02();
   }
 
 }
