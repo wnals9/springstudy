22ffc729fd94c12762926e553f369fa114055a1c
wnals9 <wnals16@naver.com>
2023-11-02 10:48:59 +0900

    20231102

--- 15_myhome/src/main/java/com/gdu/myhome/dto/UploadDto.java
+++ 15_myhome/src/main/java/com/gdu/myhome/dto/UploadDto.java
@@ -10,11 +10,11 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class UploadDto {
-  
   private int uploadNo;
   private String title;
   private String contents;
   private String createdAt;
   private String modifiedAt;
+  private int attachCount;  // UPLOAD_T에는 없는 칼럼이지만, 목록 보기에서 첨부파일개수를 반환하므로 그 결과를 저장하기 위해서 추가함
   private UserDto userDto;  // private int userNo;
 }
